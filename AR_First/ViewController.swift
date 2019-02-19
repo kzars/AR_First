d59b02f83bc612ac30ee38fbf58e9482d278db2c
Kārlis Zars <zarskarlis@gmail.com>
2019-02-19 21:32:05 +0200

    Sphere objects with texture and 3D model example added

--- AR_First/ViewController.swift
+++ AR_First/ViewController.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2018 Kārlis Zars. All rights reserved.
 //
 
+//Sphere texture taken from https://www.solarsystemscope.com/textures/
+//3D models taken from https://www.turbosquid.com
+
 import UIKit
 import SceneKit
 import ARKit
@@ -21,13 +24,65 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        //sceneView.showsStatistics = true
+
+        let sphere = SCNSphere(radius: 0.2)
+        let sphere2 = SCNSphere(radius: 0.3)
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        //Example with cube
+        //let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+
+        let material = SCNMaterial()
+        let material2 = SCNMaterial()
+
+        material2.diffuse.contents = UIImage(named: "art.scnassets/8k_earth_daymap.jpg")
+        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        //For 3D Models example
+        //material.diffuse.contents = UIImage(named: "art.scnassets/8k_venus_atmosphere.jpg")
+        
+        //Example with cube
+        //material.diffuse.contents = UIColor.red
+
+        sphere.materials = [material]
+        sphere2.materials = [material2]
+        
+        //Example with cube
+        //cube.materials = [material]
+
+        let node = SCNNode()
+        let node2 = SCNNode()
+
+        node2.position = SCNVector3(x: 0.5, y: 0.1, z: -0.1)
+        node.position = SCNVector3(x: 0, y: 0.1, z: -1)
+
+        node2.geometry = sphere2
+        node.geometry = sphere
+        
+        //Example with cube
+        //node.geometry = cube
+
+        sceneView.scene.rootNode.addChildNode(node2)
+        sceneView.scene.rootNode.addChildNode(node)
+
+
+        sceneView.autoenablesDefaultLighting = true
+        
+
+        
+        // For 3D Models example
+//        let cameraScene = SCNScene(named: "art.scnassets/collada.scn")!
+//
+//        if let cameraNode = cameraScene.rootNode.childNode(withName: "opener", recursively: true) {
+//
+//            cameraNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+//
+//            sceneView.scene.rootNode.addChildNode(cameraNode)
+//
+//        }
+//       // Set the scene to the view
+//        sceneView.scene = scene
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,29 +102,4 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
